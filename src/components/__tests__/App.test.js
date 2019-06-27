18def417db100b7297b6b111448bbdcf48a7dfe5
Phonekham <phonekham.dev@gmail.com>
2019-06-27 21:01:20 +0700

    14. Verifying Component Existence

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import App from "../App";
 
 it("it shows a comment box", () => {
-  const div = document.create("div");
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  //   look inside the div
+  //   and checks to see if the CommentCox is in there
+  expect(div.innerHTML).toContain("Comment box");
+
   ReactDOM.unmountComponentAtNode(div);
 });
