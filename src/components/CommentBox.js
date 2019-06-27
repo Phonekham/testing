4e472ef81be6b137cb056175a67a786e6ece04d5
Phonekham <phonekham.dev@gmail.com>
2019-06-27 18:49:48 +0700

    setting up

---
+++ src/components/CommentBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h3>Comment box</h3>
+    </div>
+  );
+};
---
+++ src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h3>Comment list</h3>
+    </div>
+  );
+};
