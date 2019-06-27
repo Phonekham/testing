a34c4889666c171188f53a39d7f7646bb013ad4a
Phonekham <phonekham.dev@gmail.com>
2019-06-27 20:51:47 +0700

    13. Tricking React with JSDOM

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
 
-it("it shows a comment box", () => {});
+it("it shows a comment box", () => {
+  const div = document.create("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
