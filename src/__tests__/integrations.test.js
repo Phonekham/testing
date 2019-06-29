db7e627567d926d51049c11a27880ee0cfa1fe88
Phonekham <phonekham.dev@gmail.com>
2019-06-29 10:24:38 +0700

    57. Faking Requests with Moxios

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { mount } from "enzyme";
+import moxios from "moxios";
 import Root from "Root";
 import App from "components/App";
-import { exportAllDeclaration } from "@babel/types";
+
+beforeEach(() => {
+  moxios, install();
+  moxios.stubRequest("https://jsonplaceholder.typicode.com/comments", {
+    status: 200,
+    response: [{ name: "Fetch #1" }, { name: "Fetch #2" }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it("fetch a list of comment and display them", () => {
   // attemp to render the entire app
@@ -14,5 +26,5 @@ it("fetch a list of comment and display them", () => {
   // find the fetchComments button and click it
   wrapped.find(".fetch-comments").simulate("click");
   // expect to find list of comments
-  expect(wrapped.find("li").length).toEqual(500);
+  expect(wrapped.find("li").length).toEqual(2);
 });
