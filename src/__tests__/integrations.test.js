1b99f50e7604907063db6055096d4ae1addb57d6
Phonekham <phonekham.dev@gmail.com>
2019-06-29 10:47:58 +0700

    60. Moxios's Wait Function

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -5,8 +5,8 @@ import Root from "Root";
 import App from "components/App";
 
 beforeEach(() => {
-  moxios, install();
-  moxios.stubRequest("https://jsonplaceholder.typicode.com/comments", {
+  moxios.install();
+  moxios.stubRequest("http://jsonplaceholder.typicode.com/comments", {
     status: 200,
     response: [{ name: "Fetch #1" }, { name: "Fetch #2" }]
   });
@@ -16,7 +16,7 @@ afterEach(() => {
   moxios.uninstall();
 });
 
-it("fetch a list of comment and display them", () => {
+it("fetch a list of comment and display them", done => {
   // attemp to render the entire app
   const wrapped = mount(
     <Root>
@@ -26,5 +26,11 @@ it("fetch a list of comment and display them", () => {
   // find the fetchComments button and click it
   wrapped.find(".fetch-comments").simulate("click");
   // expect to find list of comments
-  expect(wrapped.find("li").length).toEqual(2);
+  //   expect(wrapped.find("li").length).toEqual(2);
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find("li").length).toEqual(2);
+    done();
+    wrapped.unmount();
+  });
 });
