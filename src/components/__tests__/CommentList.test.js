eacb8f222c5fc284073c5094266d4ea8741fe5d3
Phonekham <phonekham.dev@gmail.com>
2019-06-29 08:58:55 +0700

    47. Redux Initial State

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -6,11 +6,16 @@ import Root from "Root";
 let wrapped;
 
 beforeEach(() => {
+  const initialState = {
+    comments: ["c1", "c2"]
+  };
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   );
 });
 
-it("Create one li per comment", () => {});
+it("Create one li per comment", () => {
+  console.log(wrapped.find("li").length);
+});
