7f939e4ca55164ea91ec5de7ef0d1c24b88c4dc8
Phonekham <phonekham.dev@gmail.com>
2019-06-30 11:03:17 +0700

    13. Placing Reusable Logic

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 
 export default ChildComponent => {
   class ComposesComponent extends React.Component {
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        this.props.history.push("/");
+      }
+    }
     render() {
       return <ChildComponent />;
     }
   }
 
-  return ComposesComponent;
+  function mapStateToProps(state) {
+    return { auth: state.auth };
+  }
+
+  return connect(mapStateToProps)(ComposesComponent);
 };
