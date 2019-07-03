28afe75e408655f268c93a115b3fbfb634232493
Phonekham <phonekham.dev@gmail.com>
2019-07-03 16:37:46 +0700

    5. Forwarding Actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,1 +1,10 @@
-export default ({ dispatch }) => next => action => {};
+export default ({ dispatch }) => next => action => {
+  // check to see if the action
+  // has a promise on its 'payload' property
+  // if it does, then wait for it to resolve
+  // if it doesn't, then send the action on to the
+  // next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+};
